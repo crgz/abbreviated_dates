8bddb111742c397ab0a23d2b19c10c32fc59617a
Conrado M. Rodriguez <5125929+crgz@users.noreply.github.com>
2021-09-22 17:36:34 +0200

    Fix Title

--- pack.pl
+++ pack.pl
@@ -3,7 +3,7 @@ name(abbreviated_dates).
 version('0.0.2').
 download('https://github.com/crgz/abbreviated_dates/releases/*.zip').
 
-title('Logical arithmetic on dates and times').
+title('Parses abbreviated & incomplete dates in multiple languages').
 author('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 maintainer('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 packager('Conrado M. Rodriguez','conrado.rgz@gmail.com').
