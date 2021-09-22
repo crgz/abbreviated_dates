7cdb263f5f0285564b28615c6a6ba66ceef8831d
Conrado M. Rodriguez <5125929+crgz@users.noreply.github.com>
2021-09-22 17:41:11 +0200

    Bump version: 0.0.2 → 0.0.3

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(abbreviated_dates).
 
-version('0.0.2').
+version('0.0.3').
 download('https://github.com/crgz/abbreviated_dates/releases/*.zip').
 
 title('Parses abbreviated & incomplete dates in multiple languages').
