28032966a8cb8405adb53ef01f730aebe68be29a
Conrado M. Rodriguez <5125929+crgz@users.noreply.github.com>
2021-09-23 08:53:50 +0200

    Fix Warning: Invalid package description: requires(tap,date_time)

--- pack.pl
+++ pack.pl
@@ -9,4 +9,5 @@ maintainer('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 packager('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 
 home('https://github.com/crgz/abbreviated_dates').
-requires(tap, date_time).
+requires(tap).
+requires(date_time).
