39277bd1e3fe5c9a81d631a22e9f566e171674a0
Conrado M. Rodriguez <5125929+crgz@users.noreply.github.com>
2021-09-29 20:21:08 +0200

    Fix Warning:    '/home/m/.local/share/swi-prolog/pack/abbreviated_dates/prolog/facts/languages.pl':5:2: Illegal multibyte Sequence

--- prolog/facts/languages.pl
+++ prolog/facts/languages.pl
@@ -3,136 +3,163 @@ language('Bulgarian',
 	['Понеделник', 'Вторник', 'Сряда', 'Четвъртък', 'Петък', 'Събота', 'Неделя'],
 	'Днес', 'Утре'
 ).
+
 language('Chinese',
 	['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
 	['ᎤᎾᏙᏓᏉᏅ', 'ᏔᎵᏁ ᎢᎦ', 'ᏦᎢᏁ ᎢᎦ', 'ᏅᎩᏁ ᎢᎦ', 'ᏧᏅᎩᎶᏍᏗ', 'ᎤᎾᏙᏓᏈᏕᎾ', 'ᎤᎾᏙᏓᏆᏍᎬ'],
 	'Avui', 'Demà'
 ).
+
 language('Croatian',
 	['Siječanj', 'Veljača', 'Ožujak', 'Travanj', 'Svibanj', 'Lipanj', 'Srpanj', 'Kolovoz', 'Rujan', 'Listopad', 'Studeni', 'Prosinac'],
 	['Ponedjeljak', 'Utorak', 'Srijeda', 'Četvrtak', 'Petak', 'Subota', 'Nedjelja'],
 	'Danas', 'Sutra'
 ).
+
 language('Czech',
 	['Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec'],
 	['Pondĕlí', 'Úterý', 'Středa', 'Čtvrtek', 'Pátek', 'Sobota', 'Nedĕle'],
 	'Dnes', 'Zítra'
 ).
+
 language('Danish',
 	['Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'December'],
 	['Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag'],
 	'I Dag', 'I Morgen'
 ).
+
 language('Dutch',
 	['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'],
 	['Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag'],
 	'Vandaag', 'Morgen'
 ).
+
 language('English',
 	['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
 	['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
 	'Today', 'Tomorrow'
 ).
+
 language('Finnish',
 	['Tammikuu', 'Helmikuu', 'Maaliskuu', 'Huhtikuu', 'Saattaa', 'Kesäkuu', 'Heinäkuu', 'Elokuu', 'Syyskuu', 'Lokakuu', 'Marraskuu', 'Joulukuu'],
 	['Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai', 'Sunnuntai'],
 	'Tänään', 'Huomenna'
 ).
+
 language('French',
 	['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
 	['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'],
 	'Aujourd\'hui', 'Demain'
 ).
+
 language('German',
 	['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
 	['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'],
 	'Heute', 'Morgen'
 ).
+
 language('Greek',
 	['Ιανουάριος', 'Φεβρουάριος', 'Μάρτιος', 'Απρίλιος', 'Μάιος', 'Ιούνιος', 'Ιούλιος', 'Αύγουστος', 'Σεπτέμβριος', 'Οκτώβριος', 'Νοέμβριος', 'Δεκέμβριος'],
 	['Mendich', 'Denschdich', 'Mittich', 'Donnerschtich', 'Fraidich', 'Samschdich', 'Sonndich'],
 	'Σήμερα', 'Αύριο'
 ).
+
 language('Hebrew',
 	['ינואר', 'פברואר', 'מרץ', 'אפריל', 'מאי', 'יוני', 'יולי', 'אוגוסט', 'ספטמבר', 'אוקטובר', 'נובמבר', 'דצמבר'],
 	['יום שני', 'יום שלישי', 'יום רביעי', 'יום חמישי', 'יום שישי', 'יום שבת', 'יום ראשון'],
 	'Σήμερα', 'Αύριο'
 ).
+
 language('Hungarian',
 	['Január', 'Február', 'Március', 'Április', 'Május', 'Június', 'Július', 'Augusztus', 'Szeptember', 'Október', 'November', 'December'],
 	['Hétfő', 'Kedd', 'Szerda', 'Csütörtök', 'Péntek', 'Szombat', 'Vasárnap'],
 	'Ma', 'Holnap'
 ).
+
 language('Italian',
 	['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
 	['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica'],
 	'Oggi', 'Domani'
 ).
+
 language('Japanese',
 	['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
 	['月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日', '日曜日'],
 	'Oggi', 'Domani'
 ).
+
 language('Lithuanian',
 	['Sausis', 'Vasaris', 'Kovas', 'Balandis', 'Gegužė', 'Birželis', 'Liepa', 'Rugpjūtis', 'Rugsėjis', 'Spalis', 'Lapkritis', 'Gruodis'],
 	['Pirmadienis', 'Antradienis', 'Trečiadienis', 'Ketvirtadienis', 'Penktadienis', 'Šeštadienis', 'Sekmadienis'],
 	'Rytoj', 'Šiandien'
 ).
+
 language('Norwegian',
 	['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember'],
 	['Lundi', 'Mardi', 'Mercrédi', 'Jéeudi', 'Vendrédi', 'Sammedi', 'Dîmmaunche'],
 	'I Dag', 'I Morgen'
 ).
+
 language('Polish',
 	['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'],
 	['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'],
 	'Dzisiaj', 'Jutro'
 ).
+
 language('Portuguese',
 	['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
 	['Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado', 'Domingo'],
 	'Hoje', 'Amanhã'
 ).
+
 language('Romanian',
 	['Ianuarie', 'Februarie', 'Martie', 'Aprilie', 'Mai', 'Iunie', 'Iulie', 'August', 'Septembrie', 'Octombrie', 'Noiembrie', 'Decembrie'],
 	['Luni', 'Marţi', 'Miercuri', 'Joi', 'Vineri', 'Sîmbătă', 'Duminică'],
 	'Astăzi', 'Mâine'
 ).
+
 language('Russian',
 	['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
 	['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
 	'Cегодня', 'Завтра'
 ).
+
 language('Swedish',
 	['Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December'],
 	['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag', 'Söndag'],
 	'I Dag', 'I Morgon'
 ).
+
 language('Slovak',
 	['Január', 'Február', 'Marec', 'Apríl', 'Máj', 'Jún', 'Júl', 'August', 'Septembra', 'Október', 'November', 'December'],
 	['Pondelok', 'Utorok', 'Streda', 'Štvrtok', 'Piatok', 'Sobota', 'Nedel\'A'],
 	'Dnes', 'Zajtra'
 ).
+
 language('Slovenian',
 	['Januar', 'Februar', 'Marec', 'April', 'Maj', 'Junij', 'Julij', 'Avgust', 'September', 'Oktober', 'November', 'December'],
 	['Ponedeljek', 'Torek', 'Sreda', 'Četrtek', 'Petek', 'Sobota', 'Nedelja'],
 	'Danes', 'Jutri'
 ).
+
 language('Spanish',
 	['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
 	['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
 	'Hoy', 'Mañana'
 ).
+
 language('Turkish',
 	['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık'],
 	['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar'],
 	'Бүген', 'Иртәгә'
 ).
+
 language('Ukrainian',
 	['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень'],
 	['Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П\'Ятниця', 'Субота', 'Неділя'],
 	'Сьогодні', 'Завтра'
 ).
+
 language('Vietnamese',
 	['Tháng Một', 'Tháng Hai', 'Diễu Hành', 'Tháng Tư', 'Có Thể', 'Tháng Sáu', 'Tháng Bảy', 'Uy Nghi', 'Tháng Chín', 'Tháng Mười', 'Tháng Mười Một', 'Tháng Mười Hai'],
 	['Thứ Hai (次𠄩)', 'Thứ Ba (次𠀧)', 'Thứ Tư (次四)', 'Thứ Năm (次𠄼)', 'Thứ Sáu (次𦒹)', 'Thứ Bảy (次𦉱)', 'Chủ Nhật (主日)'],
