da0f8700966ccd7a695b2ae4f0d3409623e73e63
Conrado M. Rodriguez <5125929+crgz@users.noreply.github.com>
2021-09-29 21:30:05 +0200

    Cleanup

--- pack.pl
+++ pack.pl
@@ -1,12 +1,12 @@
 name(abbreviated_dates).
-
-version('0.0.20').
-download('https://github.com/crgz/abbreviated_dates/releases/*.zip').
-
 title('Parses abbreviated & incomplete dates in multiple languages').
+version('0.0.20').
+
 author('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 maintainer('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 packager('Conrado M. Rodriguez','conrado.rgz@gmail.com').
+
+download('https://github.com/crgz/abbreviated_dates/releases/*.zip').
 
 home('https://github.com/crgz/abbreviated_dates').
 requires(tap).
