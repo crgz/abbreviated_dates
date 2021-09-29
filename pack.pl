c9ab3084c077944c1daeeb8feaecff4e072641c7
Conrado M. Rodriguez <5125929+crgz@users.noreply.github.com>
2021-09-29 21:33:06 +0200

    Test change download url

--- pack.pl
+++ pack.pl
@@ -6,7 +6,7 @@ author('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 maintainer('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 packager('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 
-download('https://github.com/crgz/abbreviated_dates/releases/*.zip').
+download('https://github.com/crgz/abbreviated_dates.git').
 
 home('https://github.com/crgz/abbreviated_dates').
 requires(tap).
