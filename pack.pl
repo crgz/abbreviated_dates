721fdeda7d3799643ec50264b53ae4d61f0cd8b2
Conrado M. Rodriguez <5125929+crgz@users.noreply.github.com>
2021-09-30 08:25:19 +0200

    Revert "Test change download url"
    This reverts commit c9ab3084c077944c1daeeb8feaecff4e072641c7.

--- pack.pl
+++ pack.pl
@@ -6,7 +6,7 @@ author('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 maintainer('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 packager('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 
-download('https://github.com/crgz/abbreviated_dates.git').
+download('https://github.com/crgz/abbreviated_dates/releases/*.zip').
 
 home('https://github.com/crgz/abbreviated_dates').
 requires(tap).
