e2d6c3115c3ca96b464d8908173a8ffd58018907
conrado.rgz <conrado.rgz@gmail.com>
2022-04-14 19:09:31 +0200

    Refactor to simplify abbreviations handling
    - Syntax Modelled after POSIX strftime()

--- prolog/abbreviated_dates.pl
+++ prolog/abbreviated_dates.pl
@@ -16,81 +16,116 @@
 :- [facts/languages]. % Facts about languages
 
 % 
-% date_time:date_get(today, Today), parse(Today, 'Freitag, 7. Mai', Dates, T).
+% date_time:date_get(today, Today), parse(Today, 'Freitag, 7. Mai', Dates, Syntax).
 % 
 % 
-parse(Context, Expression, Dates, Trace) :-
-  atom_codes(Expression, Codes), phrase(multipleDays([Context], Dates, Trace), Codes).
+parse(Context, Expression, Dates, [Language, Syntax]) :-
+  atom_codes(Expression, Codes), phrase(multiple_days([Context], Dates, Language, Syntax), Codes).
 
-multipleDays(_, [], []) --> [].
-multipleDays([LastKnownDate|Other], [SingleDay|MultipleDays], [T1|T2]) -->
-  singleDay([LastKnownDate|Other], SingleDay, T1),
+%-----------------------------------------------------------
+% Grammar
+%
+multiple_days(_, [], _, []) --> [].
+multiple_days([LastKnownDate|Other], [SingleDay|MultipleDays], Language, [S1|S2]) -->
+  single_day([LastKnownDate|Other], SingleDay, Language, S1),
   (dash | eos),
-  multipleDays([SingleDay, LastKnownDate|Other], MultipleDays, T2).
+  multiple_days([SingleDay, LastKnownDate|Other], MultipleDays, Language, S2).
 
-singleDay([Context|_], date(Y, M, D), dm(Language)) -->
-  dayNumber(D), b, month(M, Language), {bestYear(Context, M, D, Y)}.
+% phrase(abbreviated_dates:single_day([date(2020, 2, 28)], Date, Language, Syntax), `1 July`).
+single_day([Context|_], date(Y, M, D), Language, ['%d', Syntax]) -->
+  day_number(D), b, month(M, Language, Syntax), 
+  {maybe_future_year(Context, M, D, Y)}.
 
-singleDay([Context|_], Date, d(unknown)) -->
-  dayNumber(D), {bestDate(Context, D, Date)}.
+% phrase(abbreviated_dates:single_day([date(2020, 2, 28)], Date, Language, Syntax), `31`).
+single_day([Context|_], Date, Language, '%d') --> 
+  day_number(D), 
+  {future_date(Context, D, Date), language(Language)}.
 
-singleDay([Context|_], date(Y, M, D), md(Language)) -->
-  month(M, Language), b, dayNumber(D), {bestYear(Context, M, D, Y)}.
-
-singleDay([Context|_], Date, adverb(Language)) -->
+% phrase(abbreviated_dates:single_day([date(2020, 2, 29)], Date, Language, Syntax), `Tomorrow`).
+single_day([Context|_], Date, Language, adverb) -->
   nonblanks(Codes),
   {atom_codes(Adverb, Codes), adverb(Language, Adverb, Context, Date)}.
 
-singleDay(Context, Date, sd(Language, T)) -->
-  weekDay(_, Language), b, singleDay(Context, Date, T).
+% phrase(abbreviated_dates:single_day([date(2020, 2, 28)], Date, Language, Syntax), `Saturday, 1 July`).
+single_day(Context, Date, Language, ['%A',Syntax]) -->
+  string(Codes), ",", b, single_day(Context, Date, Language, Syntax),
+  {atom_codes(WeekDay, Codes), dayName(Language, _, WeekDay)}.
 
-weekDay(D, wd(Language)) -->
-  string(Codes), ",",
-  {atom_codes(WeekDay, Codes), dayName(Language, D, WeekDay)}.
+% phrase(abbreviated_dates:single_day([date(2020, 2, 28)], Date, Language, Syntax), `Jan. 1`).
+single_day([Context|_], date(Y, M, D), Language, [MonthFormat, '%d']) -->
+  month(M, Language, MonthFormat), b, day_number(D), 
+  {maybe_future_year(Context, M, D, Y)}.
 
-dayNumber(D) --> integer(D).
-dayNumber(D) --> integer(D), ".".
+day_number(D) --> integer(D).
+day_number(D) --> integer(D), ".".
 
-month(MonthNumber, explicit(Language)) -->
+month(MonthNumber, Language, '%B') --> % explicit month
   nonblanks(Codes),
-  { atom_codes(MonthName, Codes), month(Language, MonthNumber , MonthName) }.
+  { atom_codes(MonthName, Codes), monthName(Language, MonthNumber , MonthName) }.
 
-month(MonthNumber, abbreviated(Language)) -->
-  string(Abbreviation), end_of_abbreviation,
-  { month(Language, MonthNumber, MonthName),
-    maybe_uppercase(MonthName, MaybeUppercase),
-    abbreviation(MaybeUppercase, Abbreviation)
+month(MonthNumber, Language, '%b') --> % abbreviated month
+  string(Abbreviation), ".",
+  { 
+    atom_codes(Prefix, Abbreviation),
+    monthName(Language, MonthNumber, MonthName),
+    sub_atom(MonthName, 0, _, _, Prefix)
   }.
-
-end_of_abbreviation --> ".".
-end_of_abbreviation --> [].
-
-maybe_uppercase(Atom, Atom).
-maybe_uppercase(Atom, MaybeUppercase):- upcase_atom(Atom, MaybeUppercase).
-
-abbreviation(Text, Abbreviation):-
-  atom_codes(Text, Codes),
-  append(Abbreviation, _, Codes),
-  length(Abbreviation,AbbreviationLength),
-  length(Codes,TextLength),
-  AbbreviationLength > 0,
-  AbbreviationLength < TextLength.
-
-bestDate(date(Year, 12, D), Day, date(BestYear, BestMonth, Day)):-
-  ( Day >= D -> BestYear = Year, BestMonth = 12; BestYear is Year + 1, BestMonth = 1).
-
-bestDate(date(Year, Month, D), Day, date(Year, BestMonth, Day)):-
-  ( Day >= D -> BestMonth = Month; BestMonth is Month + 1).
-
-bestYear(Context, Month, Day, Year) :-
-  date_extract(Context, years(Y)),
-  ( date_compare(date(Y, Month, Day), >=, Context) -> Year = Y; Year is Y + 1).
 
 dash --> " - ".
 b --> white.
 
+%-----------------------------------------------------------
+% Internal predicates
+%
+future_date(date(Y, M, D), Day, date(YY,MM,DD)):-
+  date_month_days(M,Y,MD),
+  D =< Day, Day =< MD, % Day in current month
+  !,
+  future_date(date(Y,M,Day), date(YY,MM,DD)).
+future_date(date(Y, M, D), Day, date(YY,MM,DD)):-
+  date_month_days(M,Y,MD),
+  (Day =< D; MD =< Day),  % Day out of current month
+  !,
+  M2 is M + 1,
+  future_date(date(Y,M2,Day), date(YY,MM,DD)).
+future_date(date(Y, M, D), date(YY,MM,DD)):-
+  M > 12,
+  !,
+  M2 is M - 12,
+  Y2 is Y + 1,
+  future_date(date(Y2,M2,D), date(YY,MM,DD)).
+future_date(date(Y,M,D), date(Y,M,D)).
 
-month(Language, MonthNumber, MonthName):-
+maybe_future_year(Context, Month, Day, Year) :-
+  date_extract(Context, years(Y)),
+  ( date_compare(date(Y, Month, Day), >=, Context) -> Year = Y; Year is Y + 1).
+
+date_month_days(0,_,31).
+date_month_days(1,_,31).
+date_month_days(2,Y,29) :- date_leap_year(Y), !.
+date_month_days(2,_,28).
+date_month_days(3,_,31).
+date_month_days(4,_,30).
+date_month_days(5,_,31).
+date_month_days(6,_,30).
+date_month_days(7,_,31).
+date_month_days(8,_,31).
+date_month_days(9,_,30).
+date_month_days(10,_,31).
+date_month_days(11,_,30).
+date_month_days(12,_,31).
+date_month_days(13,_,31).
+
+date_leap_year(Y) :-
+   ( ( 0 =:= Y mod 100, 0 =:= Y mod 400 ) ;
+     ( 0 =\= Y mod 100, 0 =:= Y mod 4 ) ).
+
+%   
+% Accessors
+%
+language(Language):- language(Language, _, _, _, _).
+
+monthName(Language, MonthNumber, MonthName):-
 	language(Language, Months, _, _, _),
 	nth1(MonthNumber, Months, MonthName).
   
--- test/test.pl
+++ test/test.pl
@@ -11,12 +11,42 @@ term_expansion(Query -> Result, (Head :- Test)) :-
 
 :- use_module(library(tap)).
 
-% Use Cases
+% Use Cases 	█
 
-parse(date(2021, 9, 21), 'Freitag, 7. Mai', Dates, Trace) ->
-    Dates = [date(2022, 5, 7)],
-    Trace = [sd(wd('German'), dm(explicit('German')))].
 
-parse(date(2021, 9, 21), '23 Sze. - 27 Sze.', Dates, Trace) ->
-    Dates = [date(2021, 9, 23), date(2021, 9, 27)],
-    Trace = [dm(abbreviated('Hungarian')), dm(abbreviated('Hungarian'))] .
+
+parse(date(2021, 9, 21), '4 July', Dates, Syntax) ->
+    Dates = [date(2022, 7, 4)],
+    Syntax = ['English',  [['%d', '%B']]].
+
+parse(date(2021, 9, 21), 'July 4', Dates, Syntax) ->
+   Dates = [date(2022, 7, 4)],
+   Syntax = ['English', [['%B', '%d']]].
+
+parse(date(2020, 2, 28), 'Saturday, 2', Dates, Syntax) ->
+   Dates = [date(2020, 3, 2)],
+   Syntax = ['English', [['%A', '%d']]].
+
+parse(date(2021, 9, 21), 'Friday, 7. May', Dates, Syntax) ->
+   Dates = [date(2022, 5, 7)],
+   Syntax = ['English', [['%A', ['%d', '%B']]]].
+
+parse(date(2020, 2, 28), '23 Sep.', Dates, Syntax) ->
+   Dates = [date(2020, 9, 23)],
+   Syntax = ['English', [['%d', '%b']]].
+
+parse(date(2020, 2, 28), '23 Sep. - 27 Sep.', Dates, Syntax) ->
+   Dates = [date(2020, 9, 23), date(2020, 9, 27)],
+   Syntax = ['English', [['%d', '%b'], ['%d', '%b']]].
+
+parse(date(2021, 9, 21), 'Today', Dates, Syntax) ->
+   Dates = [date(2021, 9, 21)],
+   Syntax = ['English', [adverb]].
+
+parse(date(2020, 2, 29), 'Tomorrow', Dates, Syntax) ->
+   Dates = [date(2020, 3, 1)],
+   Syntax = ['English', [adverb]].
+
+parse(date(2021, 9, 21), '4', Dates, Syntax) ->
+   Dates = [date(2021, 10, 4)],
+   Syntax = ['English', ['%d']].
