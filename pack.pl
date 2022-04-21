380d2259b28e5c19415e4319eb7c109a97c7e8de
conrado.rgz <conrado.rgz@gmail.com>
2022-04-21 21:56:56 +0200

    Reset version to match https://www.swi-prolog.org/pack/list?p=abbreviated_dates

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(abbreviated_dates).
 title('Parses abbreviated & incomplete dates in multiple languages').
-version('1.0.47').
+version('0.0.44').
 
 author('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 maintainer('Conrado M. Rodriguez','conrado.rgz@gmail.com').
