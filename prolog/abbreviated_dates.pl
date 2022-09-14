0ec02473ad66471b994ef217833f3aa5b0e7e79d
Jeffrey Johnson <jeffrey.johnson@zalando.de>
2022-09-14 11:57:39 +0200

    Fix weekday with comma e.g. (sri, 21. 09.)

--- prolog/abbreviated_dates.pl
+++ prolog/abbreviated_dates.pl
@@ -124,6 +124,8 @@ month(MonthNumber, Language, '%b') --> % abbreviated month
 
 week_day(InputCodes) --> % abbreviated week day
   string_without(". ", InputCodes), optional_period.
+week_day(InputCodes) --> % abbreviated week day
+  string_without("., ", InputCodes), optional_period.
 
 month_day(Day) --> integer(Day), {between(1, 31, Day)}.
 date_number(N) --> integer(N).
