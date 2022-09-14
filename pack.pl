90ab6b7b3a0dfd81fec3cd7cab271c502837cac1
corodriguez <conrado.rodriguez@zalando.de>
2022-09-14 16:43:27 +0200

    Bump version: 0.0.120 → 0.0.121

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(abbreviated_dates).
 title('Parses abbreviated and ambiguous dates in multiple languages').
-version('0.0.120').
+version('0.0.121').
 
 author('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 maintainer('Conrado M. Rodriguez','conrado.rgz@gmail.com').
