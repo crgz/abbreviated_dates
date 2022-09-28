4e7cd9daf48794ac9f0f1b225dbce11d4cf735d3
Conrado Rodriguez <conrado.rgz@gmail.com>
2022-09-28 13:35:53 +0200

    Bump version: 0.0.223 → 0.0.224

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(abbreviated_dates).
 title('Parses abbreviated and ambiguous dates in multiple languages').
-version('0.0.223').
+version('0.0.224').
 
 author('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 maintainer('Conrado M. Rodriguez','conrado.rgz@gmail.com').
