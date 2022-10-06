d3051d21795e8e305114365642ee2f796a4f093c
crgz <crgz@users.noreply.github.com>
2022-10-06 19:19:21 +0000

    [create-pull-request] automated change

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(abbreviated_dates).
 title('Parses abbreviated and ambiguous dates in multiple languages').
-version('0.0.290').
+version('0.0.291').
 
 author('Conrado M. Rodriguez','conrado.rgz@gmail.com').
 maintainer('Conrado M. Rodriguez','conrado.rgz@gmail.com').
