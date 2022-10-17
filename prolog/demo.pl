cee7d46eb099bee792157ca5d30c476dc839babd
Conrado Rodriguez <conrado.rgz@gmail.com>
2022-10-17 18:08:54 +0200

    Add Key Features, improve demo

--- prolog/demo.pl
+++ prolog/demo.pl
@@ -1,9 +1,11 @@
 :- use_module(library(abbreviated_dates)).
 :- use_module(library(cli_table)).
 
-solutions(Date):-
-  findall([F,L,C],(abbreviated_dates:parse(date(2022,09,9),Date,[D],_,L,C),
-  format_time(string(F),"%A, %d %b %Y",D)),Y),
-  cli_table(Y,[head(['Date','Language','Country'])]).
+solutions(Text):- % E.g. solutions('11-09, št').
+  Starting = date(2022,09,9),
+  findall([Date,Language,Country],format(Starting,Text,Date,Language,Country),Row),
+  cli_table(Row,[head(['Date','Language','Country'])]).
 
-solutions('11-09, št').
+format(Starting, Text, DateText, Language, Country):-
+  parse(Starting, Text, [Date], _, Language, Country),
+  format_time(string(DateText), "%A, %d %b %Y", Date).
